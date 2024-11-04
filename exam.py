7e1764ef7b13571d461cbf3b2e2f1c858343b7cc
Shreeyav031 <shreeyav031@gmail.com>
2024-11-04 20:01:02 +0530

    4th

---
+++ exam.py
@@ -0,0 +1,6 @@
+num=int(input("enter your attendence to check:"))
+if num>346:
+    print("you are not eligible for the examination")
+    if num<346:
+     print("you are eligible for the examination")
+  
