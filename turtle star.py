ef7db00e2b0d3286f55583ce57b8a8d420c42b30
Shreeyav031 <shreeyav031@gmail.com>
2024-11-07 21:34:51 +0530

    7th nov

---
+++ turtle star.py
@@ -0,0 +1,27 @@
+import turtle
+sc=turtle.Screen()
+sc.bgcolor("orange")
+sc.setup(700,700)
+sc.title("TURTLE library")
+point=turtle.Turtle()
+point.forward(200)
+point.left(120)
+point.forward(200)
+point.left(120)
+point.forward(200)
+point.penup()
+point.right(150)
+point.forward(120)
+point.pendown()
+point.right(90)
+point.forward(200)
+point.right(120)
+point.forward(200)
+point.right(120)
+point.forward(200)
+
+
+
+
+
+turtle.done()
---
+++ turtle_1.py
@@ -0,0 +1,20 @@
+import turtle
+sc=turtle.Screen()
+sc.bgcolor("purple")
+sc.setup(700,700)
+sc.title("TURTLE library")
+point=turtle.Turtle()
+point.forward(200)
+point.left(90)
+point.forward(200)
+point.left(90)
+point.forward(200)
+point.left(90)
+point.forward(200)
+
+
+
+
+
+
+turtle.done()
